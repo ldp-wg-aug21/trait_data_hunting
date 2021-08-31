4483e454716cf5d70db9e1a0da7c52f25b20eda4
Sandra Emry <sandraemry@Sandras-MacBook-Pro.local>
2021-08-30 21:46:27 -0700

    1st stab at extracting some trait data from fishbase

---
+++ scripts/fishbase_data_extraction.R
@@ -0,0 +1,37 @@
+## Extracting trait data from Fishbase
+
+#load packages
+pkgs <- c("tidyverse", "rfishbase")
+lapply(pkgs, library, character.only = TRUE)
+rm(pkgs)
+
+# load Canadian LPI data
+data <- read_csv("./data-raw/CIEE_LPI_dataset.csv")
+
+# filter out fish only
+fish_LPI <- data %>% 
+  filter(Class == "Fish")
+
+# what species do we have abundance data for?
+fish_species <- unique(fish_LPI$Binomial)
+
+#replace '_' with ' ' to match fishbase format
+fish_species <- gsub("_", " ", fish_species)
+
+#trying out the ecology() function in rfishbase to get trophic level and habitat information
+fish_ecology <- ecology(species_list = fish_species,
+                        fields = c("Species", "Herbivory2", "DietTroph", "DietSeTroph", 
+                                   "DietRemark"))
+
+fish_ecology <- fish_ecology %>% 
+  rename(Binomial = Species) 
+
+fish_ecology$Binomial <- gsub(" ", "_", fish_ecology$Binomial)
+
+fish_ecology$trophic_database <- "fishbase"
+
+# join new clumns back with fish_LPI data set 
+fish_LPI <- left_join(x = fish_LPI, y = fish_ecology, by = "Binomial")
+
+
+
