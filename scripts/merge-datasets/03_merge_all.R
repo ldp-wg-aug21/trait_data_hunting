fe32561f7328dcdc3a44f8bc508553c3437a71f2
Katherine Hébert <katherine.ahebert@gmail.com>
2021-09-01 16:33:30 -0400

    initial commit

---
+++ scripts/merge-datasets/03_merge_all.R
@@ -0,0 +1,23 @@
+# Script to merge all taxon-specific datasets into one mega-dataset to rule them all
+
+# read in the taxon-specific datasets
+taxon_traits <- lapply(paste0("data-clean/", list.files("data-clean", pattern = "traits-specific-")), 
+                       read.csv, row.names = 1)
+# name each element of the list from the file names (and remove the file extension etc.)
+names(taxon_traits) <- gsub("traits-specific-", "", 
+                            list.files("data-clean", pattern = "traits-specific-")) %>%
+  gsub(".csv", "", .)
+
+# select the three standardized traits
+standard_traits <- lapply(taxon_traits, 
+                          subset, 
+                          select = c("UUID", "Binomial", 
+                                     "BodySize_std", 
+                                     "TrophicLevel", 
+                                     "LifeSpan"))
+
+# bind all datasets together into an overall trait dataset
+all_traits <- bind_rows(standard_traits, .id = "Group")
+
+# save file
+write.csv(all_traits, "data-clean/traits-all.csv")
